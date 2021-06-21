f822221ce244ccaffdd50fb30e9aa65d3e838959
Maksym <sat2151@gmail.com>
2021-06-21 11:04:23 +0300

    Added a function which hide all tab content and remove class active on the tabs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-﻿console.log('ss')
+﻿window.addEventListener('DOMContentLoaded',()=>{
+    const tabs = document.querySelectorAll('.tabheader__item')
+    const tubcontent=document.querySelectorAll('.tabcontent')
+    const tubsParent = document.querySelectorAll('.tabheader__items')
+
+    function hideAllTabContent(){
+        tubcontent.forEach((item)=>{
+            item.style.display='none';
+        })
+
+        tabs.forEach((tub)=>{
+            tub.classList.remove('tabheader__item_active')
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
