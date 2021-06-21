7b647f0964829da0a6f6a4914ca72500ecb617db
Maksym <sat2151@gmail.com>
2021-06-21 11:23:47 +0300

    Added a function which show tub and added active class on tab

--- js/script.js
+++ js/script.js
@@ -9,10 +9,15 @@
         })
 
         tabs.forEach((tub)=>{
-            tub.classList.remove('tabheader__item_active')
+            tub.classList.remove('tabheader__item_active');
         })
     }
 
+    function showTubContent(i){
+        tubcontent[i].style.display='block';
+        tubs[i].classList.classList.add('tabheader__item_active')
+
+    }
 
 
 
