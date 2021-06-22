db20a655f5fb3214517a45420985ed1c37d4652b
Maksym <sat2151@gmail.com>
2021-06-22 14:40:43 +0300

    Add two EventLintener for MODAL windoW

--- js/script.js
+++ js/script.js
@@ -91,11 +91,20 @@
 
     setClock('.timer',deadline)
 
+    // Modal window
 
+    const modalTrigger=document.querySelector('[data-modal]')
+    const modalWindow=document.querySelector('.modal')
+    const modalCloseBtn=document.querySelector('[data-close]')
 
-
-
-
-
-
+    modalTrigger.addEventListener('click',()=>{
+        modalWindow.classList.add('show')
+        modalWindow.classList.remove('hide')
+        document.body.style.overflow='hidden'
+    })
+   
+    modalCloseBtn.addEventListener('click',()=>{
+        modalWindow.classList.add('hide')
+        modalWindow.classList.remove('show')
+    })
 })
