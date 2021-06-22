3e253c8d88ad376994d814f2ce609e80a8226060
Maksym <sat2151@gmail.com>
2021-06-22 22:47:00 +0300

    Create class for cards.Added new card using class Card and method render

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 ﻿window.addEventListener('DOMContentLoaded',()=>{
-    
+   
     // Tabs
     const tabs = document.querySelectorAll('.tabheader__item')
     const tubcontent=document.querySelectorAll('.tabcontent')
@@ -137,5 +137,47 @@
 
     window.addEventListener('scroll',showModayWindowByScroll)
 
+    // Using class for Cards
+
+    class MenuCard{
+        constructor(src,alt,title,descr,price,perentSelector) {
+            this.src=src;
+            this.alt=alt;
+            this.title=title;
+            this.descr=descr;
+            this.price=price;
+            this.parent=document.querySelector(perentSelector)
+            this.transfer=27;
+            this.changeToUAH();
+        }
+
+        changeToUAH(){
+            this.price = this.price * this.transfer;
+        }
+
+        render(){
+            const element = document.createElement('div');
+            element.innerHTML=`
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>`;
+            this.parent.append(element)
+        }
+    }
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        9,
+        '.menu .container'
+    ).render() 
 
 })
