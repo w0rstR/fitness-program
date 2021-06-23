8fdcb049336318760d69404b603395e55bb2bf3b
Maksym <sat2151@gmail.com>
2021-06-23 23:33:05 +0300

    Update func postData for sending JSON format

--- js/script.js
+++ js/script.js
@@ -216,13 +216,25 @@
             statusMessage.textContent=message.loading;
             form.append(statusMessage);
 
+            // const request = new XMLHttpRequest();
+            // request.open('POST','../server.php')
+            // //request.setRequestHeader('Content-type','multipart//form-data')
+            // const formData = new FormData(form)
+
+            // FOR JSON 
             const request = new XMLHttpRequest();
 
             request.open('POST','../server.php')
-            //request.setRequestHeader('Content-type','multipart//form-data')
-            const formData = new FormData(form)
-        
-            request.send(formData)
+            request.setRequestHeader('Content-type','application/json')
+            const formData= new FormData(form);
+            
+            const object={}
+            formData.forEach(function(value,key){
+                object[key]=value
+            })
+            const json = JSON.stringify(object)
+
+            request.send(json)
             request.addEventListener('load',()=>{
                 if(request.status === 200){
                     console.log(request.response)
--- server.php
+++ server.php
@@ -1,4 +1,5 @@
 <?php
-echo var_dump($_POST);
+$_POST=json_decode(file_get_contents("php://input"),true);
 
+echo var_dump($_POST);
 ?>
